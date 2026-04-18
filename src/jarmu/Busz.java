7ff474f3b59a9c84cfc943647d7b451c5d96b809
Halfsa <szendefy.alex@petrik.hu>
2026-04-18 20:45:19 +0200

    busz pénzkeresés

--- src/jarmu/Busz.java
+++ src/jarmu/Busz.java
@@ -1,5 +1,6 @@
 package jarmu;
 
+import gazdasag.Sofor;
 import halozat.Checkpoint;
 import halozat.Csomopont;
 import vezerles.SkeletonLogger;
@@ -8,13 +9,15 @@ import vezerles.SkeletonLogger;
  * A Sofőr által irányított jármű, amely fordulók megtételével pénzt keres.
  */
 public class Busz extends IranyitottJarmu {
+    private Sofor vezeto; // A busz sofőrje, aki irányítja a járművet és felelős a pénzkeresésért
     private final Checkpoint start; // A busz kiindulási pozíciója
     private final Checkpoint cel; // A busz célállomása
     private boolean oda = true; // Jelzi, hogy a busz éppen a cél felé (oda) vagy vissza (vissza) tart-e
 
-    public Busz(Checkpoint start, Checkpoint cel) {
+    public Busz(Checkpoint start, Checkpoint cel, Sofor vezeto) {
         this.start = start;
         this.cel = cel;
+        this.vezeto = vezeto;
     }
 
     /**
@@ -31,6 +34,12 @@ public class Busz extends IranyitottJarmu {
                 aktualisCsomopont.elenged(this);
             }
             aktualisCsomopont = celCsomopont;
+            if(aktualisCsomopont.equals(cel) && oda) {
+                vezeto.keres(100); // A sofőr pénzt keres, amikor a busz eléri a célt
+                iranytValtoztat(); // A busz visszafordul a kiindulási pontra
+            }else if(aktualisCsomopont.equals(start) && !oda) {
+                iranytValtoztat(); // A busz újra elindul a cél felé
+            }
             SkeletonLogger.exit(true);
             return true;
         }else{
