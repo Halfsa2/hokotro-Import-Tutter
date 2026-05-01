103e3f039332325d368c0a0bbc87c4a8586613c4
viragbibi <232608191+viragbibi@users.noreply.github.com>
2026-05-01 19:49:19 +0200

    Tesztek 24-35 && 69-75

--- src/jarmu/Hokotro.java
+++ src/jarmu/Hokotro.java
@@ -3,6 +3,7 @@ package jarmu;
 import felszereles.Hanyofej;
 import felszereles.Jegtoro;
 import felszereles.Kotrofej;
+import felszereles.Sopro;
 import gazdasag.Takarito;
 import halozat.Csomopont;
 import halozat.Sav;
@@ -15,20 +16,21 @@ import vezerles.SkeletonLogger;
  * A Takarító által vezérelt munkagép, amely a sávok tisztításáért felel.
  */
 public class Hokotro extends IranyitottJarmu {
-    
-    private Takarito tulajdonos; 
-    private Kotrofej aktiv; 
-    private HashMap<String, Kotrofej> birtokolja; 
+
+    private Takarito tulajdonos;
+    private Kotrofej aktiv;
+    private HashMap<String, Kotrofej> birtokolja;
 
     public Hokotro(Takarito tulajdonos) {
         this.tulajdonos = tulajdonos;
         this.birtokolja = new HashMap<>();
-        addFej(new Hanyofej()); // Alapértelmezett fej, minden hókotró rendelkezik vele
+        addFej(new Sopro()); // Alapértelmezett fej, minden hókotró rendelkezik vele
         addFej(new Jegtoro());
     }
 
     /**
      * Kicseréli az aktív fejet a hókotróban.
+     * 
      * @param ujFej az új fej, amire át akar kapcsolni
      */
     public void cserelFej(Kotrofej ujFej) {
@@ -42,20 +44,22 @@ public class Hokotro extends IranyitottJarmu {
 
     /**
      * Hozzáad egy további takarítófejet a hókotró készletéhez.
+     * 
      * @param ujFej az új fej, amit hozzáad
      */
     public void addFej(Kotrofej ujFej) {
         SkeletonLogger.enter(this, "addFej", ujFej);
-        if(this.birtokolja.containsKey(ujFej.getClass().getSimpleName())) {
+        if (this.birtokolja.containsKey(ujFej.getClass().getSimpleName())) {
             SkeletonLogger.exit("void");
             return; // Már van ilyen típusú fej, nem adunk hozzá újat
         }
         this.birtokolja.put(ujFej.getClass().getSimpleName(), ujFej);
         SkeletonLogger.exit("void");
     }
-    
+
     /**
      * Getter, ami név alapján visszaad egy birtokolt fejet
+     * 
      * @param tipusNev a fej típusának neve, amit le akar kérni
      */
     public Kotrofej getFej(String tipusNev) {
@@ -64,6 +68,7 @@ public class Hokotro extends IranyitottJarmu {
 
     /**
      * Takarítja a megadott sávot a jelenleg aktív takarítófejjel.
+     * 
      * @param s a takarítandó sáv
      * @return true, ha sikerült a takarítás
      */
@@ -91,16 +96,18 @@ public class Hokotro extends IranyitottJarmu {
     @Override
     public boolean lep(Csomopont celCsomopont) {
         SkeletonLogger.enter(this, "lep", celCsomopont);
-        
+
         // 1. Várakozás (büntetés) ellenőrzése
         if (this.varakozik > 0) {
             this.varakozik--; // Eltelt egy próbálkozás / kör
-            // Ideális esetben a hibaüzenetet a JatekVezerlo írja ki, de itt adjuk vissza a false-t
+            // Ideális esetben a hibaüzenetet a JatekVezerlo írja ki, de itt adjuk vissza a
+            // false-t
             SkeletonLogger.exit(false);
             return false;
         }
 
-        // 2. Topológiai validáció: szomszédos-e a célcsomópont? (Kivéve az első lehelyezést)
+        // 2. Topológiai validáció: szomszédos-e a célcsomópont? (Kivéve az első
+        // lehelyezést)
         if (this.aktualisCsomopont != null) {
             List<Csomopont> szomszedok = this.aktualisCsomopont.getNext();
             if (szomszedok == null || !szomszedok.contains(celCsomopont)) {
@@ -118,32 +125,34 @@ public class Hokotro extends IranyitottJarmu {
             }
             // Frissítjük a pozíciót
             this.aktualisCsomopont = celCsomopont;
-            
+
             // 4. Takarítás, ha sávra léptünk
             if (aktualisCsomopont instanceof Sav sav) {
                 // Ha sikerült kitakarítani a sávot, akkor a tulajdonos pénzt keres.
-                if(this.takarit(sav)) {
+                if (this.takarit(sav)) {
                     tulajdonos.keres(5);
                 }
             }
             SkeletonLogger.exit(true);
             return true;
         }
-        
-        // Ha a befogad() false-t adott (pl. foglalt, vagy mély hó busznál), ide futunk ki
+
+        // Ha a befogad() false-t adott (pl. foglalt, vagy mély hó busznál), ide futunk
+        // ki
         SkeletonLogger.exit(false);
         return false;
     }
-    
+
     @Override
     public boolean lephetMelyHora() {
         return true; // A hókotró képes mély hóban is közlekedni
     }
+
     @Override
     public void printStat(String name) {
         System.out.print("Hokotro " + name + ": aktualisCsomopont=");
         if (this.aktualisCsomopont != null) {
-            System.out.print(aktualisCsomopont != null? reverseNevTar.get(this.aktualisCsomopont) : "null");
+            System.out.print(aktualisCsomopont != null ? reverseNevTar.get(this.aktualisCsomopont) : "null");
         }
         System.out.print(", aktivFej=" + (aktiv != null ? aktiv.getClass().getSimpleName() : "null"));
         System.out.println();
